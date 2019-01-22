96a454edad8919d98efab871f21a72f294fbfd80
Rahul Patwari <rahulpatwari233@gmail.com>
2019-01-23 00:39:32 +0530

    Use variables in place of hardcoded min, max
    Udemy: s02-10

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 	void Start () {
+		int max = 100;
+		int min = 1;
+
 		Debug.Log ("Welcome to Number wizard");
 		Debug.Log ("Pick a number");
-		Debug.Log ("The highest number you can pick is 100");
-		Debug.Log ("The lowest number you can pick is 1");
+		Debug.Log ("The highest number you can pick is: " + max);
+		Debug.Log ("The lowest number you can pick is: " + min);
 	}
 
 	void Update () {
