9bd9ecc9cfcb90d7832d8fc721710749a44a6e60
Rahul Patwari <rahulpatwari233@gmail.com>
2019-01-23 02:13:33 +0530

    Guess the number
    Udemy: s02-14

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -1,29 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
+	int max = 100;
+	int min = 1;
+	int guess;
 	void Start () {
-		int max = 100;
-		int min = 1;
 
 		Debug.Log ("Welcome to Number wizard");
 		Debug.Log ("Pick a number");
 		Debug.Log ("The highest number you can pick is: " + max);
 		Debug.Log ("The lowest number you can pick is: " + min);
 		Debug.Log ("Tell me if you number is higher or lower than my guess");
-		Debug.Log ("Push Up = Higer, Push Down = Lower, Push Enter = Correct");
+		Debug.Log ("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+		guess = (min + max) / 2;
+		printMessage (guess);
 	}
 
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			Debug.Log ("Up Arrow key was pressed");
-		}
-		if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			Debug.Log ("Down Arrow key was pressed");
-		}
 		if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.Return)) {
-			Debug.Log ("Enter Arrow key was pressed");
+			ClearLog ();
+			Debug.Log ("Yeeee!!! I guessed it. Your number is: " + guess);
+		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			min = guess;
+			guess = (min + max) / 2;
+			printMessage (guess);
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			max = guess;
+			guess = (min + max) / 2;
+			printMessage (guess);
 		}
+	}
+
+	public void printMessage (int guess) {
+		Debug.Log ("Is it higher or lower than: " + guess + "? ");
+	}
+
+	public void ClearLog () {
+		var assembly = Assembly.GetAssembly (typeof (UnityEditor.Editor));
+		var type = assembly.GetType ("UnityEditor.LogEntries");
+		var method = type.GetMethod ("Clear");
+		method.Invoke (new object (), null);
 	}
 }
