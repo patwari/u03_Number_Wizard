4b18212478efbe0a7cfeeed2ea832f5c53032394
Rahul Patwari <rpatwari@ingenuitygaming.com>
2019-01-23 01:16:04 +0530

    Get user input through keys
    Udemy: s02-11

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -11,9 +11,19 @@ public class NumberWizard : MonoBehaviour {
 		Debug.Log ("Pick a number");
 		Debug.Log ("The highest number you can pick is: " + max);
 		Debug.Log ("The lowest number you can pick is: " + min);
+		Debug.Log ("Tell me if you number is higher or lower than my guess");
+		Debug.Log ("Push Up = Higer, Push Down = Lower, Push Enter = Correct");
 	}
 
 	void Update () {
-
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			Debug.Log ("Up Arrow key was pressed");
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			Debug.Log ("Down Arrow key was pressed");
+		}
+		if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.Return)) {
+			Debug.Log ("Enter Arrow key was pressed");
+		}
 	}
 }
