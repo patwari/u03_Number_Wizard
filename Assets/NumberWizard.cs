5da8349ef1db8881a07615c1297cb72bbd09653d
Rahul Patwari <rahulpatwari233@gmail.com>
2019-01-23 00:26:55 +0530

    Print welcome message using Debug.Log
    Udemy: s02-09

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+	void Start () {
+		Debug.Log ("Welcome to Number wizard");
+		Debug.Log ("Pick a number");
+		Debug.Log ("The highest number you can pick is 100");
+		Debug.Log ("The lowest number you can pick is 1");
+	}
+
+	void Update () {
+
+	}
+}
